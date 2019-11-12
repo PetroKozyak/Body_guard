7cdbb4bfa2e5e9f721b43140bf08ec2f39767970
p.kozyak@gmail.com <p.kozyak@gmail.com>
2019-11-12 12:32:43 +0200

    -init project

---
+++ bodyguard/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(r'api-auth/', include('rest_framework.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token, name='get-token'),
+    path(r'', include('bodyguard_api.urls'))
+
+]
---
+++ bodyguard_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BodyguardApiConfig(AppConfig):
+    name = 'bodyguard_api'
